bb0b6a52b87f67418b9e4b569f9969dabe679970
Thiago Macêdo <thiago95macedo@gmail.com>
2023-11-29 22:22:05 -0400

    Login e senha na view admin ajustados (verificar duplicidade do titulo da sessão)

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import IQOption
 
-# Register your models here.
+class IQOptionInline(admin.StackedInline):
+    model = IQOption
+    can_delete = False
+    verbose_name_plural = 'IQ Option'
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super().get_formset(request, obj, **kwargs)
+        formset.can_delete = False
+        return formset
+
+class CustomUserAdmin(BaseUserAdmin):
+    inlines = (IQOptionInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -3,15 +3,19 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class UserProfile(models.Model):
+class IQOption(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    cpf = models.CharField(max_length=11, blank=True)
-    mobile = models.CharField(max_length=11, blank=True)
-    iqoption = models.BooleanField(default=False)
-    iqoption_email = models.CharField(max_length=100, blank=True)
-    iqoption_password = models.CharField(max_length=100, blank=True)
+    iqoption_permitir = models.BooleanField(verbose_name=' Permitir Operações Trader', default=False)
+    iqoption_email = models.CharField(verbose_name='E-mail', max_length=100, blank=True)
+    iqoption_password = models.CharField(verbose_name='Senha', max_length=100, blank=True)
+
+    def __str__(self):
+        return ''  # retorna uma string vazia
+    
+    def __unicode__(self):
+        return ''  # retorna uma string vazia
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
-    UserProfile.objects.get_or_create(user=instance)
-    instance.userprofile.save()
+    IQOption.objects.get_or_create(user=instance)
+    instance.iqoption.save()
