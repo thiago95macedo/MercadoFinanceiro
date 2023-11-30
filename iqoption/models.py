2fefc40325c6f7dcc932e2ed063a8413fbf10e6e
Thiago Macêdo <thiago95macedo@gmail.com>
2023-11-30 13:59:16 -0400

    Akustando strings e tipos de campo do modelo

--- iqoption/models.py
+++ iqoption/models.py
@@ -26,9 +26,9 @@ class AtivosBinarios(models.Model):
         ('USDJPY-OTC', 'USD/JPY (OTC)'),
     ]
 
-    ativo_binario = models.CharField(max_length=20, choices=ATIVO_CHOICES)
-    ativo_binario_aberto = models.BooleanField(default=False)
-    ativo_binario_m1 = models.DecimalField(max_digits=10, decimal_places=2)
-    ativo_binario_m1_lucro = models.DecimalField(max_digits=10, decimal_places=2)
-    ativo_binario_m5 = models.DecimalField(max_digits=10, decimal_places=2)
-    ativo_binario_m5_lucro = models.DecimalField(max_digits=10, decimal_places=2)
+    ativo_binario = models.CharField(verbose_name= 'Ativi Binário', max_length=20, choices=ATIVO_CHOICES)
+    ativo_binario_aberto = models.BooleanField(verbose_name= 'Aberto', default=False)
+    ativo_binario_m1 = models.BooleanField(verbose_name= 'M1', default=False)
+    ativo_binario_m1_lucro = models.DecimalField(verbose_name= 'Lucro M1', max_digits=10, decimal_places=2)
+    ativo_binario_m5 = models.BooleanField(verbose_name= 'M5', default=False)
+    ativo_binario_m5_lucro = models.DecimalField(verbose_name= 'Lucro M5', max_digits=10, decimal_places=2)
