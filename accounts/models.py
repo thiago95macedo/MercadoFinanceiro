99e49f68acfa936996e4ee9ad69779eb1772b5e0
Thiago Macêdo <thiago95macedo@gmail.com>
2023-11-30 21:32:42 -0400

    Update

--- accounts/models.py
+++ accounts/models.py
@@ -3,15 +3,19 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+# Definição do modelo IQOption
 class IQOption(models.Model):
+    # Constantes para os tipos de conta
     REAL = 'REAL'
     PRACTICE = 'PRACTICE'
 
+    # Opções para o campo de escolha do tipo de conta
     TYPE_CHOICES = [
         (REAL, 'Real'),
         (PRACTICE, 'Treinamento'),
     ]
 
+    # Campos do modelo
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     iqoption_permitir = models.BooleanField(verbose_name=' Permitir Operações Trader', default=False)
     iqoption_email = models.CharField(verbose_name='E-mail', max_length=100, blank=True)
@@ -22,13 +26,17 @@ class IQOption(models.Model):
     iqoption_practice_saldo = models.DecimalField(verbose_name='Saldo Conta Treinamento', max_digits=10, decimal_places=2, default=0)
     iqoption_practice_saldo_display = models.CharField(verbose_name='Saldo Conta Treinamento', max_length=20, default='0,00')
 
+    # Método para representação em string do objeto
     def __str__(self):
         return ''  # retorna uma string vazia
     
+    # Método para representação unicode do objeto
     def __unicode__(self):
         return ''  # retorna uma string vazia
 
+# Sinal que é disparado após um objeto User ser salvo para atuaçozação do Saldo das Contas Real e Treinamento
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     IQOption.objects.get_or_create(user=instance)
     instance.iqoption.save()
+
--- iqoption/models.py
+++ iqoption/models.py
@@ -1,32 +1,8 @@
 from django.db import models
 
 class AtivosBinarios(models.Model):
-    ATIVO_CHOICES = [
-        ('AUDCAD', 'AUD/CAD'),
-        ('AUDJPY', 'AUD/JPY'),
-        ('AUDUSD', 'AUD/USD'),
-        ('CADCHF', 'CAD/CHF'),
-        ('EURGBP', 'EUR/GBP'),
-        ('EURJPY', 'EUR/JPY'),
-        ('EURUSD', 'EUR/USD'),
-        ('GBPJPY', 'GBP/JPY'),
-        ('GBPUSD', 'GBP/USD'),
-        ('USDJPY', 'USD/JPY'),
-        ('USDCAD', 'USD/CAD'),
-        ('USDCHF', 'USD/CHF'),
-        ('XAUUSD', 'OURO'),
-        ('AUDCAD-OTC', 'AUD/CAD (OTC)'),
-        ('EURGBP-OTC', 'EUR/GBP (OTC)'),
-        ('EURJPY-OTC', 'EUR/JPY (OTC)'),
-        ('EURUSD-OTC', 'EUR/USD (OTC)'),
-        ('GBPJPY-OTC', 'GBP/JPY (OTC)'),
-        ('GBPUSD-OTC', 'GBP/USD (OTC)'),
-        ('NZDUSD-OTC', 'NZD/USD (OTC)'),
-        ('USDCHF-OTC', 'USD/CHF (OTC)'),
-        ('USDJPY-OTC', 'USD/JPY (OTC)'),
-    ]
 
-    ativo_binario = models.CharField(verbose_name= 'Ativi Binário', max_length=20, choices=ATIVO_CHOICES)
+    ativo_binario = models.CharField(verbose_name= 'Ativos Binários', max_length=20)
     ativo_binario_aberto = models.BooleanField(verbose_name= 'Aberto', default=False)
     ativo_binario_m1 = models.BooleanField(verbose_name= 'M1', default=False)
     ativo_binario_m1_lucro = models.DecimalField(verbose_name= 'Lucro M1', max_digits=10, decimal_places=2)
